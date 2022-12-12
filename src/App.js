a99502256fb378af162912087bb9de027b64fac5
bk408 <bhavyakhatri1912@gmail.com>
2022-12-13 00:25:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Bhavya";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Bhavya
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+       <div className="container">
+        <h1>Hello {name}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, cum deserunt consectetur sed quae porro veniam magni eum, accusantium facilis itaque quos dolor iusto ratione illo. Ea a voluptates qui commodi sapiente rerum adipisci!</p>
+       </div>
+    </>
+    
   );
 }
 
