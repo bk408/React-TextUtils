5688317ca1f11c7ca4002a53ea34b6ddd7018c21
bk408 <bhavyakhatri1912@gmail.com>
2022-12-14 01:02:22 +0530

    navbar add with the help of bootstrap

---
+++ module2.mjs
@@ -0,0 +1,10 @@
+const a = "Bhvaya";
+const b = "Prachi";
+const c = "Aastha";
+const d = "Shivani";
+
+
+export default b;
+export {a};
+export {c};
+export {d};
--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-let name = "Bhavya";
+import Navbar from './components/Navbar';
+
+
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-       <div className="container">
-        <h1>Hello {name}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, cum deserunt consectetur sed quae porro veniam magni eum, accusantium facilis itaque quos dolor iusto ratione illo. Ea a voluptates qui commodi sapiente rerum adipisci!</p>
-       </div>
+ <Navbar  title="TextUtils" aboutText="About us" />
+ {/*<Navbar /> */}
     </>
     
   );
