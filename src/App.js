ebb02b3c5397d9079b5865e64d35dd41c95102cc
bk408 <bhavyakhatri1912@gmail.com>
2022-12-14 21:09:19 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 
 function App() {
@@ -8,6 +9,13 @@ function App() {
     <>
  <Navbar  title="TextUtils" aboutText="About us" />
  {/*<Navbar /> */}
+
+  <div className="container my-3">
+
+ <TextForm heading="Enter the text to analyze below" />
+  </div>
+
+
     </>
     
   );
